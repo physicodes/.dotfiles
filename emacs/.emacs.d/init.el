c3f2e89a7e46ac2aa98aeec96d88ee70e7ad7bfa
Joshua Read <josh@beans.local>
2020-01-27 08:55:09 +0000

    Split custom variables into separate file

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -103,20 +103,7 @@
 ;  :after org
 ;  :config
 ;  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("2b9dc43b786e36f68a9fd4b36dd050509a0e32fe3b0a803310661edb7402b8b6" default)))
- '(inhibit-startup-screen t)
- '(org-agenda-files (quote ("~/Dropbox/Personal/org/agenda.org")))
- '(package-selected-packages (quote (gruvbox-theme use-package magit better-defaults))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+; Load from
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
